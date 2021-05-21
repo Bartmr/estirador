51d64a8950a7da5737615a667bc78243b0482b58
Bartolomeu <pbartmr@gmail.com>
2021-05-21 17:28:14 +0100

    Remove unused ClassToObjectLiteral type

--- libs/shared/src/internals/utils/types/classes-types.ts
+++ libs/shared/src/internals/utils/types/classes-types.ts
@@ -1,5 +1,3 @@
 import type { Abstract, Type } from '@nestjs/common';
 
-export type ClassToObjectLiteral<T extends {}> = { [K in keyof T]: T[K] };
-
 export type ClassType<T = unknown> = Type<T> | Abstract<T>;
