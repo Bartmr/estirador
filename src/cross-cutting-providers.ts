04d6fb84026770d25281ed60b068b4f8d0039a5b
Bartolomeu Rodrigues <pbartmr@gmail.com>
2021-07-15 19:35:25 +0100

    App interceptor is no longer needed, since nulls are not stripped anymore

--- src/cross-cutting-providers.ts
+++ src/cross-cutting-providers.ts
@@ -1,16 +1,11 @@
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { AllExceptionsFilter } from './internals/error-handling/all-exceptions.filter';
-import { AppInterceptor } from './internals/server/app.interceptor';
 import { AppValidationPipe } from './internals/validation/validation.pipe';
 
 export const CROSS_CUTTING_PROVIDERS = [
   {
     provide: APP_PIPE,
     useClass: AppValidationPipe,
-  },
-  {
-    provide: APP_INTERCEPTOR,
-    useClass: AppInterceptor,
   },
   {
     provide: APP_FILTER,
