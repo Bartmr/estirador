9a84e5fa5a0790d4b9e128eb7f2c09c2f4d6b645
Bartolomeu <pbartmr@gmail.com>
2021-08-10 12:19:21 +0100

    Removed missplaced comma in Page

--- client-side/web-app/src/components/routing/page/page.tsx
+++ client-side/web-app/src/components/routing/page/page.tsx
@@ -25,7 +25,6 @@ export function Page(props: Props) {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
-        ,
       </Head>
       <div className="min-vh-100 d-flex flex-column">
         <Header menuHtmlId="page-header-menu" className="sticky-top" />
