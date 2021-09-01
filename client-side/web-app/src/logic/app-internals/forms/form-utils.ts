a00dfbdc2143bca05632d0478c254ee292f82439
Bartolomeu <pbartmr@gmail.com>
2021-09-01 17:01:30 +0100

    form utils improvements

--- client-side/web-app/src/logic/app-internals/forms/form-utils.ts
+++ client-side/web-app/src/logic/app-internals/forms/form-utils.ts
@@ -1,28 +1,35 @@
-import { FieldError, FieldPath, UseFormReturn } from 'react-hook-form';
+import {
+  FieldError,
+  FieldPath,
+  UseFormReturn,
+  ValidateResult,
+} from 'react-hook-form';
 import get from 'lodash/get';
 
 type FormValueBase = {};
 
 type FormUtils<FormValue extends FormValueBase> = {
-  getErrorFromField: (path: FieldPath<FormValue>) => FieldError | undefined;
+  getErrorTypesFromField: (path: FieldPath<FormValue>) => {
+    [key: string]: ValidateResult;
+  };
 };
 
 export function useFormUtils<FormValue extends FormValueBase>(
   form: UseFormReturn<FormValue>,
 ) {
   const formUtils: FormUtils<FormValue> = {
-    getErrorFromField: (path) => {
+    getErrorTypesFromField: (path) => {
       const errorInField = get(form.formState.errors, path) as
         | { [key: string]: unknown }
         | FieldError
         | undefined;
 
       if (!errorInField) {
-        return undefined;
+        return {};
       } else if (typeof errorInField.type === 'string') {
-        return errorInField as FieldError;
+        return (errorInField as FieldError).types || {};
       } else {
-        return undefined;
+        return {};
       }
     },
   };
