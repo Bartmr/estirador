34e0b705cd9bb80ada79e109d5940b8eeea49de1
Bartolomeu <pbartmr@gmail.com>
2021-09-02 17:40:24 +0100

    Remove redundant navigation abstractions

--- client-side/web-app/src/components/routing/redirect/redirect.tsx
+++ client-side/web-app/src/components/routing/redirect/redirect.tsx
@@ -1,18 +1,17 @@
+import { navigate } from 'gatsby';
 import { useEffect } from 'react';
-import { useAppNavigation } from 'src/logic/app-internals/app-navigation/use-app-navigation';
 
 type Props = {
   href: string;
 };
 
 export function Redirect(props: Props) {
-  const appNavigation = useAppNavigation();
-
   useEffect(() => {
     if (props.href.includes('://')) {
       window.location.href = props.href;
     } else {
-      appNavigation.navigateWithoutAwaiting(props.href);
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      navigate(props.href);
     }
   }, []);
 
