2dfa38e6d69f8cbc75425c061b69726674db4a89
Bartolomeu <pbartmr@gmail.com>
2021-09-18 20:13:14 +0100

    Remove unnecessary environment variables modules

--- client-side/web-app/src/logic/app-internals/environment/environment-variables.schema.ts
+++
@@ -1,7 +0,0 @@
-import { object } from 'not-me/lib/schemas/object/object-schema';
-import { string } from 'not-me/lib/schemas/string/string-schema';
-
-export const ENVIRONMENT_VARIABLES_VALIDATION_SCHEMA = object({
-  SHOPIFY_STOREFRONT_ACCESS_TOKEN: string().filled(),
-  SHOPIFY_STOREFRONT_DOMAIN: string().filled(),
-}).required();
