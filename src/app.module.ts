d80c54f1323076b3563781c8356a9c6bd67c11d7
Bartolomeu <pbartmr@gmail.com>
2021-10-05 22:11:38 +0100

    Revert "Added Setting module"
    This reverts commit eb49f2418d3bc041e3e9bb843909be3134b4f0bb.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CROSS_CUTTING_PROVIDERS } from './cross-cutting-providers';
 import { LoggingModule } from './internals/logging/logging.module';
 import { LoggingServiceSingleton } from './internals/logging/logging.service.singleton';
-import { InjectConnection, TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DEFAULT_DATABASE_TYPEORM_CONNECTION_OPTIONS } from './internals/databases/default-database-typeorm-connection-options';
 import { AuthModule } from './auth/auth.module';
 import { RemoteConfigModule } from './remote-config/remote-config.module';
-import { AuditContext } from './internals/auditing/audit-context';
-import { generateUniqueUUID } from '@app/shared/internals/utils/uuid/generate-unique-uuid';
-import { ProcessContextManager } from './internals/process/process-context-manager';
-import { Connection } from 'typeorm';
-import { SettingsRepository } from './settings/typeorm/settings.repository';
 
 @Module({
   imports: [
@@ -24,20 +19,4 @@ import { SettingsRepository } from './settings/typeorm/settings.repository';
   ],
   providers: [...CROSS_CUTTING_PROVIDERS],
 })
-export class AppModule implements OnModuleInit {
-  constructor(@InjectConnection() private connection: Connection) {}
-
-  async onModuleInit() {
-    const auditContext: AuditContext = {
-      operationId: generateUniqueUUID(),
-      requestPath: undefined,
-      requestMethod: undefined,
-      authContext: undefined,
-      processId: ProcessContextManager.getContext().id,
-    };
-
-    await this.connection
-      .getCustomRepository(SettingsRepository)
-      .initializeSettings(auditContext);
-  }
-}
+export class AppModule {}
--- src/settings/typeorm/settings.entity.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleEntity } from 'src/internals/databases/simple-entity/simple.entity';
-import { Entity } from 'typeorm';
-
-@Entity()
-export class Settings extends SimpleEntity {}
