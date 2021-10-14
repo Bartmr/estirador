59abf8d6411f6bbb9f8629fefa4f784fcb4521d9
Bartolomeu <pbartmr@gmail.com>
2021-10-14 10:34:43 +0100

    WithContrastingColors component improvements

--- client-side/web-app/src/components/ui-kit/core/coloring/with-contrasting-colors.tsx
+++ client-side/web-app/src/components/ui-kit/core/coloring/with-contrasting-colors.tsx
@@ -7,49 +7,48 @@ type Props<AsHTMLElement extends keyof JSX.IntrinsicElements> = {
   dominantBackgroundColor: string;
   children?: ReactNode;
   className?: string;
+  justAttachTheColors?: boolean;
 } & JSX.IntrinsicElements[AsHTMLElement];
-
-function isColorDark(color: string): boolean {
-  const colorInstance = tinycolor(color);
-
-  if (colorInstance.isValid()) {
-    return tinycolor(color).isDark();
-  } else {
-    Logger.logError(
-      'attach-contrasting-colors:is-color-dark:invalid-color',
-      new Error(),
-      { color },
-    );
-    return false;
-  }
-}
 
 export function WithContrastingColors<
   AsHTMLElement extends keyof JSX.IntrinsicElements,
->(props: Props<AsHTMLElement>) {
-  const {
-    asHTMLElement,
-    dominantBackgroundColor,
-    children,
-    className,
-    ...htmlElementProps
-  } = props;
-
+>({
+  asHTMLElement,
+  dominantBackgroundColor,
+  children,
+  className,
+  justAttachTheColors,
+  ...htmlElementProps
+}: Props<AsHTMLElement>) {
   const As = asHTMLElement as unknown as ComponentClass;
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
   const _htmlElementProps = htmlElementProps as any;
 
-  return (
-    <As
-      {..._htmlElementProps}
-      className={`${className || ''} ${
-        isColorDark(dominantBackgroundColor)
-          ? 'with-light-contrasting-colors'
-          : 'with-dark-contrasting-colors'
-      }`}
-    >
-      {children}
-    </As>
-  );
+  const colorInstance = tinycolor(dominantBackgroundColor);
+
+  if (colorInstance.isValid()) {
+    const isDarkBackgroundColor = tinycolor(colorInstance).isDark();
+
+    return (
+      <As
+        {..._htmlElementProps}
+        className={`${className || ''} ${
+          justAttachTheColors ? 'attach' : 'with'
+        }-${
+          isDarkBackgroundColor
+            ? 'light-contrasting-colors'
+            : 'dark-contrasting-colors'
+        }`}
+      >
+        {children}
+      </As>
+    );
+  } else {
+    Logger.logError('with-contrasting-colors:invalid-color', new Error(), {
+      color: dominantBackgroundColor,
+    });
+
+    return <>{children}</>;
+  }
 }
