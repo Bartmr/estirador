7c38b28c2c8a14a1b59ac0a1d6c4cd1a74557550
Bartolomeu <pbartmr@gmail.com>
2022-01-02 23:10:54 +0000

    Do not await job functions + logging fixes

--- src/internals/jobs/run-job.ts
+++ src/internals/jobs/run-job.ts
@@ -1,11 +1,20 @@
 import 'source-map-support/register';
 import '../environment/load-environment-variables';
+import { NODE_ENV } from '../environment/node-env.constants';
+import { NodeEnv } from '../environment/node-env.types';
 
 import type { LoggingService } from '../logging/logging.service';
 import { LoggingServiceSingleton } from '../logging/logging.service.singleton';
 import { ProcessType } from '../process/process-context';
 import { ProcessContextManager } from '../process/process-context-manager';
 import { generateRandomUUID } from '../utils/generate-random-uuid';
+
+if (NODE_ENV === NodeEnv.Test) {
+  throw new Error(
+    `Test the job function without wrapping it in prepareJob() or even importing this file.
+Place the job function into a new file, and test it by importing said new file`,
+  );
+}
 
 type Dependencies = {
   loggingService: LoggingService;
@@ -20,14 +29,14 @@ type JobFunction = (dependencies: Dependencies) => Promise<void>;
 export function prepareJob(
   jobId: string,
   jobFunction: JobFunction,
-): () => Promise<void> {
+): () => void {
   ProcessContextManager.setContext({
     type: ProcessType.Job,
     name: jobId,
     workerId: generateRandomUUID(),
   });
 
-  return async (): Promise<void> => {
+  const runAsync = async (): Promise<void> => {
     let loggingService: LoggingService;
 
     try {
@@ -53,4 +62,11 @@ export function prepareJob(
       timeout.unref();
     }
   };
+
+  const run = () => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    runAsync();
+  };
+
+  return run;
 }
--- src/internals/logging/logging.service.singleton.ts
+++ src/internals/logging/logging.service.singleton.ts
@@ -1,7 +1,15 @@
 /* eslint-disable no-console */
 import { inspect } from 'util';
 import { EnvironmentVariablesService } from '../environment/environment-variables.service';
+import { NODE_ENV } from '../environment/node-env.constants';
+import { NodeEnv } from '../environment/node-env.types';
 import { LoggingService } from './logging.service';
+
+if (NODE_ENV === NodeEnv.Test) {
+  throw new Error(
+    'Do not import this logging implementation in testing environments. Use a mock instead',
+  );
+}
 
 function nestedValuesToString(value: unknown) {
   return inspect(value, undefined, 10);
