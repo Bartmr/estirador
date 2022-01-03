19766a8811a87435464bec40567daa60b8a3ce9e
Bartolomeu <pbartmr@gmail.com>
2022-01-03 15:24:08 +0000

    Improvements to fonts-ssr-apis

--- client-side/web-app/src/components/ui-kit/fonts-ssr-apis.tsx
+++ client-side/web-app/src/components/ui-kit/fonts-ssr-apis.tsx
@@ -10,13 +10,19 @@ export const attachFontsProviders = ({
   replacePostBodyComponents([
     ...elements,
     <link
-      key={'google-fonts-preconnect'}
+      key={'fonts-googleapis-preconnect'}
       rel="preconnect"
-      href="https://fonts.gstatic.com"
+      href="https://fonts.googleapis.com"
     />,
     <link
-      key={'google-fonts-stylesheet'}
-      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+      key={'fonts-gstatic-preconnect'}
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key={'font1-stylesheet'}
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
       rel="stylesheet"
     />,
   ]);
