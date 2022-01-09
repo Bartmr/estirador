2f7cc5b6d509cc4a7694e112534795e74fd68fc3
Bartolomeu <pbartmr@gmail.com>
2022-01-09 16:05:02 +0000

    TransportedDataGate no longer unmounts children on change

--- client-side/web-app/src/components/shared/transported-data-gate/transported-data-gate.tsx
+++ client-side/web-app/src/components/shared/transported-data-gate/transported-data-gate.tsx
@@ -1,6 +1,8 @@
 import {
   faExclamationCircle,
+  faLock,
   faSearch,
+  faWifi,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { ReactNode } from 'react';
@@ -32,21 +34,14 @@ export function TransportedDataGate<T extends TransportedData<unknown>>({
   className,
   loadingMessage,
 }: Props<T>) {
-  const flexClassName = `${
-    /*
-      If className exists, a wrapping div is created,
-      and the inner content must expand to its size
-    */
-    className ? 'h-100' : ''
-  } d-flex ${
+  const flexClassName = `d-flex ${
     layout === TransportedDataGateLayout.Tape
       ? 'flex-row justify-content-center align-items-center'
       : 'flex-column justify-content-center align-items-center'
   }`;
+
   const spinnerSizeClass =
-    layout === TransportedDataGateLayout.Tape
-      ? 'spinner-default'
-      : 'spinner-lg';
+    layout === TransportedDataGateLayout.Tape ? 'spinner-sm' : 'spinner-lg';
   const spinnerPaddingClass =
     layout === TransportedDataGateLayout.Tape ? '' : 'p-3';
 
@@ -56,132 +51,134 @@ export function TransportedDataGate<T extends TransportedData<unknown>>({
     layout === TransportedDataGateLayout.Tape ? 'ms-2' : 'mt-3 h4'
   }`;
 
-  const gateContent = (() => {
-    switch (dataWrapper.status) {
-      case TransportedDataStatus.NotInitialized:
-        return <div />;
-      case TransportedDataStatus.Loading:
-        return (
-          <div className={flexClassName}>
-            <div
-              className={`w-auto ${spinnerPaddingClass}`}
-              style={{ position: 'relative', zIndex: 1 }}
-            >
-              <div
-                className={`${spinnerSizeClass} spinner-border text-primary`}
-                role="status"
-              >
-                <span className="sr-only">Loading...</span>
-              </div>
-            </div>
-            {loadingMessage ? (
-              <p className={`${textClassName} text-primary mb-0`}>
-                {loadingMessage}
-              </p>
-            ) : null}
+  let gateStatusUI: ReactNode;
+
+  if (dataWrapper.status === TransportedDataStatus.NotInitialized) {
+    gateStatusUI = null;
+  } else if (dataWrapper.status === TransportedDataStatus.Loading) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <div
+          className={`w-auto ${spinnerPaddingClass}`}
+          style={{ position: 'relative', zIndex: 1 }}
+        >
+          <div
+            className={`${spinnerSizeClass} spinner-border text-primary`}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
           </div>
-        );
-      case TransportedDataStatus.Refreshing:
-        return (
-          <>
-            <div
-              className={`w-auto ${spinnerPaddingClass}`}
-              style={{
+        </div>
+        {loadingMessage ? (
+          <p className={`${textClassName} text-primary mb-0`}>
+            {loadingMessage}
+          </p>
+        ) : null}
+      </div>
+    );
+  } else if (dataWrapper.status === TransportedDataStatus.Refreshing) {
+    gateStatusUI = (
+      <div
+        style={
+          layout === TransportedDataGateLayout.Tape
+            ? {
+                marginRight: 'var(--spacer-2)',
+              }
+            : {
                 zIndex: 1,
                 right: 'var(--spacer-3)',
-                marginTop: 'var(--spacer-3)',
                 position: 'fixed',
-              }}
-            >
-              <div
-                className={`${spinnerSizeClass} spinner-border text-primary`}
-                role="status"
-              >
-                <span className="sr-only">Loading...</span>
-              </div>
-            </div>
-            {children({ data: dataWrapper.data as UnwrapTransportedData<T> })}
-          </>
-        );
+              }
+        }
+      >
+        <div
+          className={`${spinnerSizeClass} spinner-border text-primary`}
+          role="status"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  } else if (dataWrapper.status === TransportedDataStatus.Done) {
+    gateStatusUI = null;
+  } else if (dataWrapper.status === TransportFailure.NotFound) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <FontAwesomeIcon className={`${iconSizeClassName}`} icon={faSearch} />
+        <p className={`${textClassName} font-weight-bold mb-0`}>
+          404 Not Found
+        </p>
+      </div>
+    );
+  } else if (dataWrapper.status === TransportFailure.ConnectionFailure) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <FontAwesomeIcon
+          className={`text-danger ${iconSizeClassName}`}
+          icon={faWifi}
+        />
+        <p className={`${textClassName} text-danger mb-0`}>
+          No Internet. Check your connection and try again.
+        </p>
+      </div>
+    );
+  } else if (dataWrapper.status === TransportFailure.Forbidden) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <FontAwesomeIcon className={`${iconSizeClassName}`} icon={faLock} />
+        <p className={`${textClassName} mb-0`}>
+          You are not allowed to access this content
+        </p>
+      </div>
+    );
+  } else if (dataWrapper.status === TransportFailure.AbortedAndDealtWith) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <p className={`${textClassName} mb-0`}>Redirecting...</p>
+      </div>
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  } else if (dataWrapper.status === TransportFailure.UnexpectedResponse) {
+    gateStatusUI = (
+      <div className={flexClassName}>
+        <FontAwesomeIcon
+          className={`text-danger ${iconSizeClassName}`}
+          icon={faExclamationCircle}
+        />
+        <p className={`text-danger ${textClassName} mb-0`}>
+          An internal error occurred. Try again later.
+        </p>
+      </div>
+    );
+  } else {
+    Logger.logError(
+      'unknown-transported-data-status-in-transported-data-gate',
+      new Error(),
+      {
+        dataStatus: (dataWrapper as TransportedData<T>).status,
+      },
+    );
 
-      case TransportedDataStatus.Done:
-        return (
-          <>
-            {children({ data: dataWrapper.data as UnwrapTransportedData<T> })}
-          </>
-        );
-      case TransportFailure.NotFound:
-        return (
-          <div className={flexClassName}>
-            <FontAwesomeIcon
-              className={`text-primary ${iconSizeClassName}`}
-              icon={faSearch}
-            />
-            <p
-              className={`${textClassName} text-primary font-weight-bold mb-0`}
-            >
-              404 Not Found
-            </p>
-          </div>
-        );
-      case TransportFailure.ConnectionFailure:
-        return (
-          <div className={flexClassName}>
-            <FontAwesomeIcon
-              className={`text-danger ${iconSizeClassName}`}
-              icon={faExclamationCircle}
-            />
-            <p className={`${textClassName} text-danger mb-0`}>
-              Connection Error
-            </p>
-          </div>
-        );
-      case TransportFailure.Forbidden:
-        return (
-          <div className={flexClassName}>
-            <FontAwesomeIcon
-              className={`text-danger ${iconSizeClassName}`}
-              icon={faExclamationCircle}
-            />
-            <p className={`${textClassName} text-danger mb-0`}>
-              You are not allowed to access this content
-            </p>
-          </div>
-        );
-      case TransportFailure.AbortedAndDealtWith:
-        return (
-          <div className={flexClassName}>
-            <p className={`${textClassName} mb-0`}>Redirecting...</p>
-          </div>
-        );
-      case TransportFailure.UnexpectedResponse:
-        return (
-          <div className={flexClassName}>
-            <FontAwesomeIcon
-              className={`text-danger ${iconSizeClassName}`}
-              icon={faExclamationCircle}
-            />
-            <p className={`text-danger ${textClassName} mb-0`}>
-              An internal error occurred. Try again later.
-            </p>
-          </div>
-        );
-      default:
-        Logger.logError(
-          'unknown-transported-data-status-in-transported-data-gate',
-          new Error(),
-          {
-            dataStatus: (dataWrapper as TransportedData<T>).status,
-          },
-        );
+    gateStatusUI = null;
+  }
 
-        return <div />;
-    }
-  })();
+  const gateContent =
+    dataWrapper.status === TransportedDataStatus.Done ||
+    dataWrapper.status === TransportedDataStatus.Refreshing
+      ? children({ data: dataWrapper.data as UnwrapTransportedData<T> })
+      : null;
 
-  return className ? (
-    <div className={className}>{gateContent}</div>
-  ) : (
-    gateContent
+  return (
+    <div
+      className={`${
+        layout === TransportedDataGateLayout.Tape &&
+        dataWrapper.status === TransportedDataStatus.Refreshing
+          ? 'd-flex flex-row align-items-center'
+          : ''
+      } ${className || ''}`}
+    >
+      <>{gateStatusUI}</>
+      <>{gateContent}</>
+    </div>
   );
 }
