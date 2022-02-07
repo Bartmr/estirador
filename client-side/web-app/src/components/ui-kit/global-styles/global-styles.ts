8097e843d65601f4976306d811e6300d41f54e1a
Bartolomeu <pbartmr@gmail.com>
2022-02-07 14:59:20 +0000

    WIP styles split

--- client-side/web-app/src/components/ui-kit/global-styles/global-styles.ts
+++ client-side/web-app/src/components/ui-kit/global-styles/global-styles.ts
@@ -1,2 +1,66 @@
-import './index.scss';
+/* --- */
+
+import './root.scss';
+import './reboot.scss';
+import './type.scss';
+import './images.scss';
+import './containers.scss';
+import './grid.scss';
+import './tables.scss';
+import './forms.scss';
+import './buttons.scss';
+import './transitions.scss';
+import './dropdown.scss';
+import './button-group.scss';
+import './nav.scss';
+import './navbar.scss';
+import './card.scss';
+import './accordion.scss';
+import './breadcrumb.scss';
+import './pagination.scss';
+import './badge.scss';
+import './alert.scss';
+import './progress.scss';
+import './list-group.scss';
+import './close.scss';
+import './toasts.scss';
+import './modal.scss';
+import './tooltip.scss';
+import './popover.scss';
+import './carousel.scss';
+import './spinners.scss';
+import './offcanvas.scss';
+import './placeholders.scss';
+
+import './helpers';
+
+import './utilities';
+
+/* --- */
+
+import 'src/components/ui-kit/global-styles/additional-components/icon';
+
+/* --- */
+
+import 'src/components/ui-kit/global-styles/additional-modifiers/text-modifiers';
+import 'src/components/ui-kit/global-styles/additional-modifiers/card-modifiers';
+import 'src/components/ui-kit/global-styles/additional-modifiers/spinners-modifiers';
+import 'src/components/ui-kit/global-styles/additional-modifiers/navbar-modifiers';
+import 'src/components/ui-kit/global-styles/additional-modifiers/selectable-modifiers';
+
+/* --- */
+
+import 'src/components/ui-kit/global-styles/additional-utilities/background-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/text-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/link-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/shadows-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/text-shadows-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/stripes-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/user-select-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/opacity-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/z-index-utilities';
+import 'src/components/ui-kit/global-styles/additional-utilities/size-utilities';
+
+/* --- */
+
 import '@fortawesome/fontawesome-svg-core/styles.css';
