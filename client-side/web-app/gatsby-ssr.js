7aede8275e2735d5624e6625b8febf39d195648d
Bartolomeu <pbartmr@gmail.com>
2022-03-27 17:13:03 +0100

    Gatsby API changes

--- client-side/web-app/gatsby-ssr.js
+++ client-side/web-app/gatsby-ssr.js
@@ -1,15 +1,13 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+// KEEP AS TOP IMPORT
+import { RootFrame } from './src/components/root-frame/root-frame';
+
+//
 
 import React from 'react';
-import { RootFrame } from './src/components/root-frame/root-frame';
 import { onPreRenderHTML as onPreRenderHTMLImpl } from './src/on-pre-render-html';
 
-export const wrapRootElement = ({ element }) => (
-  <RootFrame>{element}</RootFrame>
-);
-
 export const onPreRenderHTML = onPreRenderHTMLImpl;
+
+export const wrapRootElement = ({ element }) => {
+  return <RootFrame>{element}</RootFrame>;
+};
