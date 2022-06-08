d77768c1b80bfa0f26343c69464192e94b729462
Bartolomeu <pbartmr@gmail.com>
2022-06-08 23:38:19 +0000

    Fix migrations test sequencer

--- test-environment-impl/migrations-tests-sequencer-impl.ts
+++ test-environment-impl/migrations-tests-sequencer-impl.ts
@@ -1,5 +1,6 @@
 import Sequencer from '@jest/test-sequencer';
 import { Test } from 'jest-runner';
+import { throwError } from 'src/internals/utils/throw-error';
 
 export class MigrationsTestSequencer extends Sequencer {
   sort(tests: Test[]) {
@@ -14,6 +15,26 @@ export class MigrationsTestSequencer extends Sequencer {
       (which in turn will force the developer to also implement a migration rollback)
       and then the migration `up()` method.
     */
-    return copyTests.sort((testA, testB) => (testA.path > testB.path ? -1 : 1));
+    return copyTests.sort((testA, testB) => {
+      const testAPathSplit = testA.path.split('/');
+      const testAFilename =
+        testAPathSplit[testAPathSplit.length - 1] || throwError();
+      const testATimestamp = Number(testAFilename.split('-')[0]);
+
+      if (isNaN(testATimestamp)) {
+        throw new Error(testA.path);
+      }
+
+      const testBPathSplit = testB.path.split('/');
+      const testBFilename =
+        testBPathSplit[testBPathSplit.length - 1] || throwError();
+      const testBTimestamp = Number(testBFilename.split('-')[0]);
+
+      if (isNaN(testBTimestamp)) {
+        throw new Error(testB.path);
+      }
+
+      return testBTimestamp - testATimestamp;
+    });
   }
 }
