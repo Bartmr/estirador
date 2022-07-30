743372468adb0bb88fce9b333ebf6bff8140ccff
Bartolomeu <pbartmr@gmail.com>
2022-07-30 11:21:48 +0000

    More fixes for VSCode debugging when using webpack

--- webpack-hmr-debug-no-type-checks.config.js
+++ webpack-hmr-debug-no-type-checks.config.js
@@ -1,5 +1,8 @@
 const webpackHMRNoTypeChecksConfig = require('./webpack-hmr-no-type-checks.config');
 
 module.exports = function (options, webpack) {
-  return webpackHMRNoTypeChecksConfig(options, webpack, ['--inspect=9229']);
+  return webpackHMRNoTypeChecksConfig(options, webpack, [
+    '--inspect=9229',
+    '--nolazy',
+  ]);
 };
