2f53d25414620278debc5380493de1c6c180eadd
Bartolomeu <pbartmr@gmail.com>
2022-09-19 05:56:05 +0000

    Remove toJSON as a way to hide fields from frontend

--- src/internals/databases/simple-entity/simple-entity.repository.ts
+++ src/internals/databases/simple-entity/simple-entity.repository.ts
@@ -81,7 +81,7 @@ export abstract class SimpleEntityRepository<
 > extends AbstractRepository<Entity> {
   _EntityCreationAttributes!: Omit<
     Entity,
-    keyof SimpleEntity | FieldsOmittedBeforePersistence | 'toJSON'
+    keyof SimpleEntity | FieldsOmittedBeforePersistence
   >;
 
   findOne(
